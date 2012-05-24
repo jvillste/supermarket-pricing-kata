7f79b660e15b83dd500f5a3900069422c8c88830
Jukka Villstedt <jukka.villstedt@gmail.com>
2012-05-24 21:07:37 +0300

    started the fourth one

--- src/shopping_chart/core.clj
+++ src/shopping_chart/core.clj
@@ -1,10 +1,36 @@
 (ns shopping-chart.core)
 
-(defn cost [products]
-  (reduce + (map :price products)))
+(defprotocol CartLine
+  (price [cart-line])
+  (receipt-line [cart-line]))
 
+(defrecord QuantityProductCartLine [name quantity offer-amount unit-price]
+  CartLine
 
-(defn receipt [products]
-  {:products products
-   :total (cost products)})
+  (price [quantity-product-cart-line] (let [{:keys [quantity offer-amount unit-price]} quantity-product-cart-line]
+                                        (* quantity
+                                           unit-price)))
 
+  (receipt-line [quantity-product-cart-line] (str (:name quantity-product-cart-line)
+                                                  " "
+                                                  (:price quantity-product-cart-line))))
+
+(defrecord WeightProductCartLine [name price-by-weight weight]
+  CartLine
+
+  (price [weight-product-cart-line] (* (:price-by-weight weight-product-cart-line)
+                                       (:weight weight-product-cart-line)))
+
+  (receipt-line [weight-product-cart-line] (str (:name weight-product-cart-line)
+                                                " "
+                                                (price weight-product-cart-line)
+                                                "$ / "
+                                                (:weight weight-product-cart-line)
+                                                " pounds")))
+(defn total [cart-lines]
+  (reduce + (map price cart-lines)))
+
+(defn receipt-lines [cart-lines]
+  (concat (map receipt-line
+               cart-lines)
+          [(str "Total " (total cart-lines))]))
--- test/shopping_chart/test/core.clj
+++ test/shopping_chart/test/core.clj
@@ -2,16 +2,32 @@
   (:use [midje.sweet])
   (:use [shopping-chart.core]))
 
+(def cart-lines [(map->QuantityProductCartLine :name "Loaf of bread"
+                                               :quantity 1
+                                               :offer-amount 10
+                                               :unit-price 1)
+                 (->QuantityProductCartLine :name "Noodles"
+                                            :quantity 1
+                                            :offer-amount 10
+                                            :unit-price 0.5)
+                 
+                 (->QuantityProductCartLine :name "Soup cans"
+                                            :quantity 1
+                                            :offer-amount 10
+                                            :unit-price 2)
+                 (->WeightProductCartLine "Apples" 2 1)])
 
-(def products [{:name "Loaf of bread" :price 1}
-               {:name "Noodles" :price 0.5}
-               {:name "Soup cans" :price 2}])
+(fact (price (->QuantityProductCartLine "Loaf of bread" 1))
+  => 1)
 
-(fact (cost products) => 3.5)
+(fact (receipt-line (->QuantityProductCartLine "Loaf of bread" 1))
+  => "Loaf of bread 1")
 
-(fact (receipt products)=>
-  {:products [{:name "Loaf of bread" :price 1}
-              {:name "Noodles" :price 0.5}
-              {:name "Soup cans" :price 2}]
-   :total 3.5})
+(fact (total cart-lines) => 5.5)
 
+(fact (receipt-lines cart-lines) =>
+  '("Loaf of bread 1"
+    "Noodles 0.5"
+    "Soup cans 2"
+    "Apples 2$ / 1 pounds"
+    "Total 5.5"))
